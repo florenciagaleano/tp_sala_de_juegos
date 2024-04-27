75f27016d21de929273610a28426409d02de4088
fgaleano <florgal2001@gmail.com>
2024-04-27 03:31:41 -0300

    Arreglar el about me

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { HomeComponent } from './components/home/home.component';
+import { QuienSoyComponent } from './components/quien-soy/quien-soy.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: "full" },
     { path: 'login', component: LoginComponent },
     { path: 'home', component: HomeComponent },
+    { path: 'quien-soy', component: QuienSoyComponent },
 
     { path: '**', component: PageNotFoundComponent},
     
