dc31cb676a2710b89fb79115667ff6d778563dfc
fgaleano <florgal2001@gmail.com>
2024-04-26 22:20:25 -0300

    Branch con el codigo original

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"tp-sala-de-juegos-e4119","appId":"1:1056489462609:web:ff318c419a7560ada75170","storageBucket":"tp-sala-de-juegos-e4119.appspot.com","apiKey":"AIzaSyDgBZCMSjqrvftUMSMYomEVTKMYRZ7_DR0","authDomain":"tp-sala-de-juegos-e4119.firebaseapp.com","messagingSenderId":"1056489462609"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore()))]
+};
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDgBZCMSjqrvftUMSMYomEVTKMYRZ7_DR0",
+  authDomain: "tp-sala-de-juegos-e4119.firebaseapp.com",
+  projectId: "tp-sala-de-juegos-e4119",
+  storageBucket: "tp-sala-de-juegos-e4119.appspot.com",
+  messagingSenderId: "1056489462609",
+  appId: "1:1056489462609:web:ff318c419a7560ada75170"
 };
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -5,6 +5,8 @@ import { CommonModule } from '@angular/common';
 import { ErrorComponent } from '../error/error.component';
 import { Router } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
+import { Firestore,addDoc,collection, collectionData } from '@angular/fire/firestore';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -16,14 +18,19 @@ import { HomeComponent } from '../home/home.component';
 export class LoginComponent {
   usuario: Usuario = new Usuario("", "");
   loginExitoso: number = -1;
+  loginsCollection: any[] = [];
+  countLogins : number = 0;
+  private sub !: Subscription;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private firestore : Firestore) {}
 
   goTo(path: string) {
     this.router.navigate([path]);
   }
 
   login(){
+    let col = collection(this.firestore,'logins');
+    addDoc(col,{ fecha: new Date(),"user":this.usuario})
     if(this.usuario.nombre == 'fgaleano' && this.usuario.clave == '1234'){
       this.loginExitoso = 1;
       this.router.navigate(['/home']);
@@ -32,6 +39,23 @@ export class LoginComponent {
       console.log("fallo");
       //this.router.navigate(['/error']);
     }
+
+  }
+
+  getData(){
+    let col = collection(this.firestore,'logins');
+    const observable = collectionData(col);
+    this.sub = observable((respuesta:any) => {
+
+      //Actualizamos nuestro array
+      this.loginsCollection = respuesta;
+
+      //Actualizamos la cantidad de registros que contiene la colección (Ejemplo propuesto en clase)
+      this.countLogins = this.loginsCollection.length;
+
+      console.log(respuesta);
+    })
+
   }
 
   closeError(){
