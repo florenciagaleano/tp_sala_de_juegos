08d2a79253a0ddfa445067ee390fabab6978c5d8
fgaleano <florgal2001@gmail.com>
2024-04-28 21:47:17 -0300

    Registro de login en firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,9 @@ import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from './environmentConfig';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"tp-sala-de-juegos-e4119","appId":"1:1056489462609:web:ff318c419a7560ada75170","storageBucket":"tp-sala-de-juegos-e4119.appspot.com","apiKey":"AIzaSyDgBZCMSjqrvftUMSMYomEVTKMYRZ7_DR0","authDomain":"tp-sala-de-juegos-e4119.firebaseapp.com","messagingSenderId":"1056489462609"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore()))]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp(environment))), 
+  importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
+import { addDoc, collection, collectionData, Firestore } from '@angular/fire/firestore';
+import { BehaviorSubject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -16,14 +18,17 @@ export class LoginComponent {
   usuario: Usuario = new Usuario("", "");
   loginExitoso: number = -1;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private firestore : Firestore) {}
 
   goTo(path: string) {
     this.router.navigate([path]);
   }
 
   login(){
-    if(this.usuario.nombre == 'fgaleano' && this.usuario.clave == '1234'){
+    let col = collection(this.firestore, 'usuarios');
+
+  if(this.usuario.nombre == 'fgaleano' && this.usuario.clave == '1234'){
+     addDoc(col, { fecha: new Date(), "usuario": this.usuario.nombre})
       this.loginExitoso = 1;
       this.router.navigate(['/home']);
     }else{
